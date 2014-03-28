58b0afdc578a667d12e1e27b7653422fc6b5be16
cameron.locke <cameronlocke@gmail.com>
2014-03-28 15:14:37 +1000

    DE20 initial checkin activemq appears to fail on my fuse

---
+++ DE20/src/main/java/org.poc.rs/InitiateEndpoint.java
@@ -0,0 +1,14 @@
+package org.poc.rs;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+public class InitiateEndpoint {
+
+    @POST
+    @Path("/")
+    public void initiateOfflineRequest(String string){
+
+    }
+
+}
