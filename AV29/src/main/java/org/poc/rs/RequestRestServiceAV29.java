3203068ce639d7cc86bc3f8422dfb3c908031f88
colletts <scollett@integraltech.com.au>
2014-04-01 21:41:36 +1000

    Added working DE13 without AD lookup
    You can spam yourself now with text messages
    
    Also added skeleton for AV29 and DE23 and JBoss Datagrid install
    instructions for REST API

---
+++ AV29/src/main/java/org/poc/rs/RequestRestServiceAV29.java
@@ -0,0 +1,15 @@
+package org.poc.rs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+public class RequestRestServiceAV29 {
+
+    @GET
+    @Path("/av29")
+    public String lookupString(String input){    	    	
+    	return null;    	
+    }
+
+    
+}
